b41e08fb3b501f599d627b3f26d641f00f1218ce
Eric Marden <eric@xentek.net>
2012-09-17 00:56:21 -0500

    major refactoring
    given that the main entry point into this class if via the DSL used in
    a Lobfile, this class current implementation is meant to serve as the
    default tasks -- that is, git fetch the latest code, mark the restore
    point in the reflog, and notify something that the deploy happened
    
    stuff still needed:
    
    - before/after callbacks for the cannonical task
    - possibly a before/after for the whole process, but that might be best
      done via open class+dsl
    - probably need to figure out how to handle errors in any of the
      invidual steps, since right now it probably just blows up and will
      leave shit in an inconsistent state
    - figure out if we really need all these classes (base, deploy, config) and can't just combine
      them...

--- lib/lobstr/deploy.rb
+++ lib/lobstr/deploy.rb
@@ -1,4 +1,41 @@
 module Lobstr
   class Deploy < ::Lobstr::Base
+    def initialize(target, config_file = 'config/lobstr.yml', &block)
+      @branch,@environment = parse_target(target)
+      @config = Lobstr::Config.new(config_file).parse(@environment)
+      return self.instance_eval(&block) if block_given?
+      deploy
+    end
+
+    def deploy
+      puts "deploying #{@branch} to #{@environment}"
+      connect do
+        update
+        puts notify
+      end
+    end
+    
+    def update
+      puts "pull the latest from #{@config['repos']}"
+      remote_task "cd #{@config['path']}"
+      remote_task "git fetch origin"
+      remote_task "git reset --hard #{@branch}"
+      puts "setting restore point"
+      remote_task "cd #{@config['path']}"
+      remote_task 'git reflog delete --rewrite HEAD@{1}'
+      remote_task 'git reflog delete --rewrite HEAD@{1}'    
+    end
+
+    def rollback
+      puts "rollback deployment"
+      remote_task "cd #{@config['path']}"
+      remote_task "git fetch origin"
+      remote_task "git reset --hard HEAD@{1}"
+    end
+
+    def notify(event = :deployment)
+      "notify of #{event}"
+    end
+
   end
 end
