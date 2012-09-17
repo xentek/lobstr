9328aae7b3af71afd36a49a25c8fae4501fa0791
Eric Marden <eric@xentek.net>
2012-09-17 00:38:45 -0500

    creates dsl entry point
    created simple dsl entry point for the class to enable syntax like this:
    
    ````
    lobstr do
      connect do
        deploy
      end
    end
    ````
    
    Behind the scenes it initializes a new instance of Lobstr:Deploy

--- lib/lobstr.rb
+++ lib/lobstr.rb
@@ -1,9 +1,19 @@
 # -*- encoding: utf-8 -*-
 
+require 'yaml'
+require 'net/ssh'
+require 'highline/import'
 require 'lobstr/base'
 require 'lobstr/config'
 require 'lobstr/deploy'
 require 'lobstr/error'
 require 'lobstr/version'
 
-module Lobstr ; end
+module Lobstr
+  class << self ; attr_accessor :target ; end
+end
+
+def lobstr(target = nil, config_file = 'config/lobstr.yml', &block)
+  target ||= Lobstr.target
+  Lobstr::Deploy.new(target, config_file, &block)
+end
