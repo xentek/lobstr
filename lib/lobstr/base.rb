d11baf06a39ea56fdf16bef1885a10d30e414595
Eric Marden <eric@xentek.net>
2012-09-17 00:41:05 -0500

    progress!
    - houses all attr_accessors for Deploy class
    - adds a connection method that takes a block... all remote tasks will
      be run inside
    - adds remote_task method that uses @ssh.exec! to run arbitrary
      commands. Right now, so output is captured.
    - adds a (currently unused) local_task method for running stuff on the
      machine deploy is initiated on.

--- lib/lobstr/base.rb
+++ lib/lobstr/base.rb
@@ -1,5 +1,6 @@
 module Lobstr
   class Base
+    attr_accessor :branch, :environment, :config, :ssh
     def parse_target(target)
       branch,environment = target.split('@', 2)
  
@@ -11,6 +12,22 @@ module Lobstr
       branch = "master" if branch.empty? # e.g. @production
       environment = "production" if environment.empty? # e.g. master@
       [branch,environment]
+    end
+
+    def connect(&block)
+      keys = [@config['ssh_key']]
+      ::Net::SSH.start(@config['ssh_host'], @config['ssh_user'], :keys => keys) do |ssh|
+        @ssh = ssh
+        instance_eval(&block)
+      end
+    end
+
+    def remote_task(cmd)
+      @ssh.exec! cmd
+    end
+
+    def local_task(cmd)
+      `#{cmd}`
     end
   end
 end
