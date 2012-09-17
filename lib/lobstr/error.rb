75c0bbc2b2802867f5856388cd630bcfc687233d
Eric Marden <eric@xentek.net>
2012-09-17 01:00:50 -0500

    adds InvalidEnvironment error class
    - if the config file can't be loaded because the environment doesn't
      exist, then throw this error

--- lib/lobstr/error.rb
+++ lib/lobstr/error.rb
@@ -1,13 +1,19 @@
 module Lobstr
   module Error
 
-    class ConfigFileExists < RuntimeError
+    class InvalidEnvironment < StandardError
+      def initialize(environment)
+        super "Invalid Environment: #{environment}. Can't load config."
+      end
+    end
+
+    class ConfigFileExists < StandardError
       def initialize(config_file)
         super "#{config_file} already exists"
       end
     end
 
-    class ConfigFileMissing < RuntimeError
+    class ConfigFileMissing < StandardError
       def initialize(config_file)
         super "#{config_file} is missing. try: \n\n$ lob config --init"
       end
